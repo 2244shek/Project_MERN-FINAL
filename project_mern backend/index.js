04831c50df57844dac611abbd93718c580955e3f
2244shek <abhishekpanigrahi2244@gmail.com>
2023-11-18 21:35:51 +0530

    first commit

---
+++ project_mern backend/index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const registerRoute = require('./routes/register');
+const loginRoute = require('./routes/login');
+const eventRoute = require("./routes/eventRoute");
+
+const app = express();
+
+// Include the User model
+// const User = require('./models/User');
+
+app.use(cors());
+const PORT = 3001;
+
+// Connect to MongoDb ...
+mongoose.connect("mongodb+srv://orbitguyasdf:Atlas2244@cluster01.u90eefs.mongodb.net/Users", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}, () => {
+    console.log("Database Connected !!...");
+})
+
+// Middleware
+app.use(bodyParser.json());
+
+// Routes
+app.use("/api/events", eventRoute);
+app.use('/register', registerRoute);
+app.use('/login', loginRoute);
+
+// Start the server
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
